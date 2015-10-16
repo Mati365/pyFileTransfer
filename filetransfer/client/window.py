9cb01de7e8a9d0b6606d39a88d4fe41a5444f51e
Mateusz Bagiński <cziken58@gmail.com>
2015-10-16 13:54:14 +0200

    Add file treeview to GTK window

--- filetransfer/client/window.py
+++ filetransfer/client/window.py
@@ -1,3 +1,5 @@
+import urllib
+
 from gi.repository import Gtk, Gdk
 from urllib.parse import urlparse
 
@@ -5,12 +7,34 @@ from ..core.p2p import P2PClient
 
 class AppWindow(Gtk.Window):
     def __init__(self):
-        super(AppWindow, self).__init__(title='FileTransfer')
+        super(AppWindow, self).__init__(title="FileTransfer")
+
+        self.set_resizable(False)
+        self.set_size_request(500, 500)
+        self.set_border_width(10)
 
         self.__configure_drag_drop()
+        self.__create_layout()
+
         self.connect("delete-event", Gtk.main_quit)
-        self.show()
+        self.show_all()
         Gtk.main()
+
+    def __create_layout(self):
+        h_box = Gtk.Box(spacing=6)
+        self.add(h_box)
+
+        scrolled_window = Gtk.ScrolledWindow()
+        scrolled_window.set_policy(Gtk.PolicyType.AUTOMATIC, Gtk.PolicyType.AUTOMATIC)
+
+        self.files = Gtk.TreeView(Gtk.ListStore(str, str, str))
+        self.files.set_size_request(-1, 128)
+        self.files.append_column(Gtk.TreeViewColumn("Size", Gtk.CellRendererText(), text=0))
+        self.files.append_column(Gtk.TreeViewColumn("File", Gtk.CellRendererText(), text=1))
+        self.files.append_column(Gtk.TreeViewColumn("Directory", Gtk.CellRendererText(), text=2))
+
+        scrolled_window.add(self.files)
+        h_box.pack_start(scrolled_window, True, True, 0)
 
     def __configure_drag_drop(self):
         self.drag_dest_set(0, [], 0)
@@ -24,5 +48,14 @@ class AppWindow(Gtk.Window):
         self.connect("drag-data-received", self.__on_drag_data_received)
 
     def __on_drag_data_received(self, widget, drag_context, x, y, data, info, time):
-        for url in data.get_data().decode("UTF-8").splitlines():
-            print(P2PClient.list_files(urlparse(url).path))
+        def get_text_size(bytes):
+            for unit in ["B", "KB", "MB", "GB"]:
+                if bytes < 1024.0:
+                    return "{0:.2f}{1}".format(bytes, unit)
+                bytes /= 1024.0
+
+        model = self.files.get_model()
+        for url in urllib.parse.unquote(data.get_data().decode("UTF-8")).splitlines():
+            files = P2PClient.list_files(urlparse(url).path)
+            for file in files[0]:
+                model.append([get_text_size(file[1]), file[0], files[2]])
